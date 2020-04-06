b3c207edb6a27cad6e48e9082dba1f6d8e9197e9
Nathan <nateim3@gmail.com>
2020-04-06 00:34:25 -0400

    Added Hangfire to the project

--- CheckLinksConsole/LinksDb.cs
+++ CheckLinksConsole/LinksDb.cs
@@ -6,13 +6,25 @@ namespace CheckLinksConsole
 {
     public class LinksDb : DbContext
     {
-	public DbSet<LinkCheckResult> Links { get; set; }
+		public DbSet<LinkCheckResult> Links { get; set; }
 
-	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-	{
-		var connection = @"Server=192.168.1.87;Database=Links;User Id=sa; Password=whatever12!";
-		optionsBuilder.UseSqlServer(connection);
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			// MSSQL:
+			var connection = @"Server=192.168.1.87;Database=Links;User Id=sa; Password=whatever12!";
+			optionsBuilder.UseSqlServer(connection);
+
+			// MySQL (Pomelo)
+			//var connection = "server=192.168.1.87;userid=root;pwd=password;database=Links;sslmode=none;";
+			//optionsBuilder.UseMySql(connection);
+
+			// PostgreSQL (Npgsql)
+			//var connection = "Host=192.168.1.87;Database=Links;Username=postgres;Password=password";
+			//optionsBuilder.UseNpgsql(connection);
+
+			// SQLite:
+			// var databaseLocation = Path.Combine(Directory.GetCurrentDirectory(), "links.db");
+			// optionsBuilder.UseSqlite($"Filename={databaseLocation}");
+		}
 	}
-
-    }
 }
--- CheckLinksConsole/Program.cs
+++ CheckLinksConsole/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Sinks;
+using Hangfire;
+using Hangfire.MemoryStorage;
 
 namespace CheckLinksConsole
 {
@@ -12,27 +14,39 @@ namespace CheckLinksConsole
     {
         static void Main(string[] args)
         {
-            var config = new Config(args); 
-            var log = Logs.CreateLogger(config);
-            log.Information($"Saving report to {config.Output.GetReportDirectory()}");
+            GlobalConfiguration.Configuration.UseMemoryStorage();
 
-            HttpClient client = new HttpClient();
-            var body = client.GetStringAsync(config.Site);
-            log.Information(body.Result);
+            RecurringJob.AddOrUpdate(() => Console.WriteLine("\n\nRecurring Job\n\n"), Cron.Minutely);
 
-            var links = LinkChecker.GetLinks(body.Result);
-            var checkedLinks = LinkChecker.CheckLinks(links);
-            // using (var file = File.CreateText(config.Output.GetReportDirectory()))
-	        using (var linksDb = new LinksDb())
-	        {
-                foreach (var link in checkedLinks.OrderBy(l => l.Exists))
-                {
-                    // var status = link.IsMissing ? "missing" : "OK";
-                    // file.WriteLine($"{status} - {link.Link}");
-		            linksDb.Links.Add(link);
-                }
-		        linksDb.SaveChanges();
+            using (var server = new BackgroundJobServer())
+            {
+                Console.WriteLine("Hangfire Server started. Press any key to exit...");
+                Console.ReadLine();
             }
+            return;
+
+         //   var config = new Config(args); 
+         //   var log = Logs.CreateLogger(config);
+         //   log.Information($"Saving report to {config.Output.GetReportDirectory()}");
+
+         //   HttpClient client = new HttpClient();
+         //   var body = client.GetStringAsync(config.Site);
+         //   log.Information(body.Result);
+
+         //   var links = LinkChecker.GetLinks(body.Result);
+         //   var checkedLinks = LinkChecker.CheckLinks(links);
+         //   // using (var file = File.CreateText(config.Output.GetReportDirectory()))
+         //   using (var linksDb = new LinksDb())
+	        //{
+         //       foreach (var link in checkedLinks.OrderBy(l => l.Exists))
+         //       {
+         //           // var status = link.IsMissing ? "missing" : "OK";
+         //           // file.WriteLine($"{status} - {link.Link}");
+		       //     linksDb.Links.Add(link);
+         //       }
+		       // linksDb.SaveChanges();
+         //       Console.WriteLine("DB Changes written");
+         //   }
         }
     }
 }
