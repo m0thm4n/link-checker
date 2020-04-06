97970e73ca3814a45676a81298279b7f5b8bcc91
Nathan <nateim3@gmail.com>
2020-04-06 08:55:45 -0400

    Hangfire now working properly

--- CheckLinksConsole/Program.cs
+++ CheckLinksConsole/Program.cs
@@ -3,6 +3,7 @@ using Hangfire;
 using Hangfire.MemoryStorage;
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
+using Hangfire.SqlServer;
 
 namespace CheckLinksConsole
 {
@@ -15,23 +16,23 @@ namespace CheckLinksConsole
            var log = Logs.CreateLogger(config);
 
 
-            GlobalConfiguration.Configuration.UseMemoryStorage();
-
-            RecurringJob.AddOrUpdate<CheckLinkJob>("check-link", j => j.Execute(config.Site, config.Output), Cron.Minutely);
-            RecurringJob.Trigger("check-link");
-
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .Build();
-            
-            
+                
+            JobStorage.Current = new SqlServerStorage(@"Server=192.168.1.87;Database=Links;User Id=sa; Password=whatever12!");
+
+
+            RecurringJob.AddOrUpdate(() => System.Console.WriteLine("Simple!"), Cron.Minutely);  
+            // RecurringJob.AddOrUpdate<CheckLinkJob>("check-link", j => j.Execute(config.Site, config.Output), Cron.Minutely);
+            // RecurringJob.Trigger("check-link");
             using (var server = new BackgroundJobServer())
             {
-                Console.WriteLine("Hangfire Server started.");
+                Console.WriteLine("Hangfire Server started. Press any key to exit...");
                 host.Run();
-            }  
+            }
         }
     }
 }
--- CheckLinksConsole/Startup.cs
+++ CheckLinksConsole/Startup.cs
@@ -2,19 +2,24 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Hangfire;
+using Hangfire.MemoryStorage;
+using Microsoft.AspNetCore.Http;
 
 namespace CheckLinksConsole
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection service)
+        public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddHangfire(c => c.UseMemoryStorage());
         }
 
-        public void Configure(IApplicationBuilder application, IHostingEnvironment env, ILoggerFactory factory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory factory)
         {
-
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
+            app.Run(async context => await context.Response.WriteAsync("We are doing well"));
         }
     }
 }
